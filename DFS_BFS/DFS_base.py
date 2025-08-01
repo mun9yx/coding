37cf058807fbb3ef93077fc5bb86febba7d94f51
mun9yx <161320181+mun9yx@users.noreply.github.com>
2025-08-01 11:37:18 +0900

    Update DFS_base.py

--- DFS_BFS/DFS_base.py
+++ DFS_BFS/DFS_base.py
@@ -1,10 +1,10 @@
 def dfs(graph,i,visited):
-    visited[i]=True
-    v = i
-    print(v,end= ' ')
-    for i in graph[v]:
-        if visited[i]==False:
-            dfs(graph,i,visited)
+    visited[i]=True #재귀할 때마다 노드 방문시 True
+    v = i #현재 노드 표시
+    print(v,end= ' ') #방문한 현재 노드 출력
+    for i in graph[v]: #현재 노드기준으로 인접 노드 순회
+        if visited[i]==False: #인접 노드가 미방문 노드라면
+            dfs(graph,i,visited) #해당 노드 탐색
 
 graph = [
     [],
